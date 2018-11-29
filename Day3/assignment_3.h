45cbfc629496d5069c9a6e47a5bd677f5a38d709
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-11-29 15:06:15 +0530

    Continued with assignment 3 - Wrote and tested: #6 - Left with: #1, #2-#5, #7-#10

--- Day3/assignment_3.h
+++ Day3/assignment_3.h
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #define allo (char *) malloc(sizeof(char) * 100)
 
+typedef enum boolean {false, true} bool;
+
 // Q2: Write a C program that will replace a calloc() function to malloc() function with appropriate parameters.
 //      E.g.- 
 //      Input: calloc(5,sizeof(float)), 
@@ -29,6 +31,44 @@ char* calToMal(char *str)
     test[testI] = '\0';
 
     return test;
+}
+
+
+// Q6: Write a C program that will check whether the input string is containing “Monday” in it.
+
+bool hasMonday(char *str)
+{
+
+    char *temp = allo;
+    char *word = "Monday";
+    int pos, tPos;
+
+    pos = tPos = -1;
+    while(str[++pos])
+    {
+        // when you reach the end
+        if (str[pos+1] == '\0')
+        {
+            temp[++tPos] = str[pos];
+            temp[++tPos] = '\0';
+            if (! strcmp(temp, word))
+                return true;
+            return false;
+        }
+
+        // when you are anywhere but the end
+        if ((int) str[pos] == 32)
+        {
+            temp[++tPos] = '\0';
+            if (! strcmp(temp, word))
+                return true;
+            free(temp);
+            temp = allo;
+            tPos = -1;
+            continue;
+        }
+        temp[++tPos] = str[pos];
+    }
 }
 
 
--- Day3/test_3.c
+++ Day3/test_3.c
@@ -14,6 +14,23 @@ int main(void)
     for (i = 0; i < 3; i++)
         printf("%s\n", calToMal(test_cases_2[i]));
     
-    
+    printf("\n");
+
+    // TEST: hasMonday
+    char *test_cases_6[] = {
+        "Monday - 1 Day",
+        "Tuesday - 2 Day",
+        "Wednesday - when, what day?",
+        "Thursday! The third day.",
+        "Satuday night, big night, date night, SA-TUR-DAY night",
+        "It's Sunday! It's God's day."
+    };
+    for (i = 0; i < 6; i++)
+    {
+        hasMonday(test_cases_6[i])
+            ? printf("%s has Monday\n", test_cases_6[i])
+            : printf("%s does not have Monday\n", test_cases_6[i]);
+    }
+
     return 0;
 }
