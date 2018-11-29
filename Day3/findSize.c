0218c49a25069ac0ab8ec752d251ec453c16c7df
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-11-29 19:59:13 +0530

    Test file for Q3

---
+++ Day3/findSize.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+	float f1;
+	float f2 = 3.5;
+	float f3 = 12.7;
+	char ch = 'a';
+	double d = 3.1416;
+	double e = 2.7132304;
+	long l = 12133;
+
+	return 0;
+}
