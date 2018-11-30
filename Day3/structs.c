3082d9b874beb2a2d88d4e7cb22e9c82207b4a0e
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-11-30 22:44:43 +0530

    Test file for Q5

---
+++ Day3/structs.c
@@ -0,0 +1,18 @@
+struct car
+{
+	int tyres;
+	char *make;
+	notAType n;
+}
+
+typedef struct myTypes types
+{
+	float f;
+	int i;
+	imnotatype n;
+	char c;
+	double d;
+	long l;
+	short s;
+	bool b; // bool is not an inbuilt datatype in C
+}
